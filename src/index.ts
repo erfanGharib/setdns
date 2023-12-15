f56777905845494b717f524aadbedf5dfe917373
Erfan Gharib <erfangharib5@gmail.com>
2023-12-15 15:43:12 +0330

    feat: update main file

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,38 @@
-process.title = 'setdns'
-console.log('hello world')
+#!/usr/bin/env node
+
+import os from "os";
+import { exec } from "child_process";
+import { runCli } from "./cli.js";
+import { __dirname } from "./global.js";
+
+const checkPrivilege = () => {
+    new Promise((resolve) => {
+        const command = "net session";
+
+        exec(command, (err, stdout) => {
+            if (
+                err ||
+                !(stdout.indexOf("There are no entries in the list.") > -1)
+            ) {
+                console.log("Run command as administrator");
+                process.exit(1);
+            }
+            resolve("");
+        });
+    });
+}
+
+const checkOS = () => {
+    if (os.platform() !== "win32") {
+        console.error("Windows supported only!");
+        process.exit();
+    }
+}
+
+;(function () {
+    process.title = "setdns";
+
+    checkOS();
+    checkPrivilege();
+    runCli();
+})();
