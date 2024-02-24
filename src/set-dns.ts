49d6c988f6a131a84a2d6b38971e29d8a01e33fa
Erfan Gharib <erfangharib5@gmail.com>
2024-02-24 13:50:55 +0330

    fix(set-dns): restructure SetDns class

--- src/set-dns.ts
+++ src/set-dns.ts
@@ -1,16 +1,18 @@
 import fs from "fs";
-import { savedDnssPath } from "./global.js";
+import { ipRegex, savedDnssPath } from "./global.js";
 import { exec } from "child_process";
 
 type T_Dns = {
-    name: string,
-    dnss: string[],
+    [key: string]: string[],
 }
 
 class SetDns {
-    private allDnss: T_Dns[] = [];
+    private allDnss: T_Dns = {};
+    countRun: number;
 
-    constructor() {}
+    constructor() {
+        this.countRun = 0;
+    }
 
     async getNetInterfaces(): Promise<string[]> {
         return new Promise((resolve, reject) => {
@@ -18,12 +20,12 @@ class SetDns {
                 if (err || stderr) {
                     reject(err || stderr);
                 }
-            
+
                 const netInterfaces = (
                     stdout
                         .split('\n')
                         .filter(v => v.includes(':\r'))
-                        .map(v => 
+                        .map(v =>
                             v
                             .substring(0, v.length - 2)
                             .substring(v.indexOf(' ', 9)+1, v.length)
@@ -34,28 +36,45 @@ class SetDns {
             });
         });
     }
-    
+
+    countDnss(netInterface: string): Promise<number> {
+        const command = `netsh interface ipv4 show dnsservers "${netInterface}"`;
+
+        return new Promise((resolve) => {
+            exec(command, (error, stdout) => {
+                const dnss = stdout.match(/\b(?:\d{1,3}\.){3}\d{1,3}\b/g) ?? [];
+                resolve(dnss.length);
+            })
+        })
+    }
+
     add(dnsName: string, dnss: string) {
         const _dnss = dnss.split(',').map(v => v.trim());
 
-        if(!_dnss.every(v => v.match(/^(\d{1,3}\.){3}\d{1,3}$/))) {
+        if(!_dnss.every(v => v.match(ipRegex))) {
             console.error('Invalid Ip Addresses\n');
             return false;
         }
 
         this.allDnss = this.readFile();
-        this.allDnss.push({ name: dnsName, dnss: _dnss });
+        this.allDnss[dnsName] = _dnss;
         this.writeFile();
 
         return true;
     }
+
+    remove(dnsName: string) {
+        this.allDnss = this.readFile();
+        delete this.allDnss[dnsName];
+        this.writeFile();
+    }
+
     async clearDnss(netInterface: string) {
         const command = `netsh interface ipv4 delete dnsservers name="${netInterface}" all`;
 
         return new Promise((resolve, reject) => {
             exec(command, (err, stdout, stderr) => {
                 if(err || stderr) {
-                    console.error(err || stderr);
                     reject('Failed to clear dns!');
                     process.exit(1);
                 }
@@ -63,29 +82,30 @@ class SetDns {
             })
         })
     }
+
     async set(dnsName: string, netInterface: string) {
         this.allDnss = this.readFile();
-
-        const whichDns = this.allDnss.filter(v => v.name === dnsName)?.[0];
-        const whichNetInterface = (await this.getNetInterfaces()).filter(v => v === netInterface)?.[0];
+        
+        const _this = this;
+        const whichDns = this.allDnss[dnsName];
 
         if(!whichDns) {
             return console.error(`Invalid Dns \`${whichDns}\`.`);
         }
 
-        if(!whichNetInterface) {
-            return console.error(`Invalid Network Interface \`${whichNetInterface}\`.`);
+        if(!netInterface) {
+            return console.error(`Invalid Network Interface \`${netInterface}\`.`);
         }
-
         return new Promise((resolve, reject) => {
-            whichDns?.dnss.forEach((DNS, index) => {
-                const command = `netsh interface ipv4 ${index === 0 ? 'set' : 'add'} dnsserver name="${whichNetInterface}" ${index === 0 ? `static ${DNS} primary` : `addr=${DNS} index=${index+1}`}`;
-
-                exec(command, (err, stdout, stderr) => {
-                    if(err || stderr) {
-                        console.error(err || stderr);
-                        reject('Failed to set dns!');
-                        process.exit(1);
+            whichDns?.forEach((DNS, index) => {
+                const command = `netsh interface ipv4 ${index === 0 ? 'set' : 'add'} dnsserver name="${netInterface}" ${index === 0 ? `static ${DNS} primary` : `addr=${DNS} index=${index+1}`}`;
+                
+                exec(command, async () => {
+                    if(await _this.countDnss(netInterface) !== whichDns.length && _this.countRun <= 4) {
+                        console.log('Failed to Set DNS. Retrying..');
+                        
+                        _this.countRun++;
+                        return _this.set(dnsName, netInterface);
                     }
 
                     resolve('\nDns set successfully!');
@@ -93,16 +113,18 @@ class SetDns {
             });
         })
     }
-    readFile() {
+
+    readFile(): T_Dns {
         if(!fs.existsSync(savedDnssPath)) {
             this.writeFile();
         }
 
         return JSON.parse(fs.readFileSync(savedDnssPath).toString());
     }
+
     writeFile() {
         fs.writeFileSync(
-            savedDnssPath, 
+            savedDnssPath,
             JSON.stringify(this.allDnss)
         )
     }
